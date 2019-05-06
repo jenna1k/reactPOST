365a1735149a3755aa4f2ee8232a79ab6b7d08a7
jenna <jenna_kim1@hotmail.com>
2019-05-06 21:32:48 +0200

    quest Form example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 import "./App.css";
-import Clock from './Clock'
-import Next from './Next'
+import Form from './Form';
 
 class App extends Component {
+  
   render() {
         return (
           <BrowserRouter>
             <div>
-              <NavLink exact to="/" activeClassName="selected" activeStyle={{color: "red"}}> Home </NavLink>
+              {/* <NavLink exact to="/" activeClassName="selected" activeStyle={{color: "red"}}> Home </NavLink>
               <NavLink to="/Next" activeClassName="selected" activeStyle={{color: "red"}}> Next </NavLink>
 
               <Switch>
                 <Route exact path="/" component={Clock} />
                 <Route path="/Next" component={Next} />
-              </Switch>
+              </Switch> */}
+              <Form />
             </div>
         </BrowserRouter>
         );
