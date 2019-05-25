5c25558a3c22617d380b2c0bb599fd2dbe89ea1e
jenna <jenna_kim1@hotmail.com>
2019-05-25 23:21:29 +0200

    deleted router part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 import "./App.css";
 import Form from './Form';
 
@@ -7,18 +6,9 @@ class App extends Component {
   
   render() {
         return (
-          <BrowserRouter>
             <div>
-              {/* <NavLink exact to="/" activeClassName="selected" activeStyle={{color: "red"}}> Home </NavLink>
-              <NavLink to="/Next" activeClassName="selected" activeStyle={{color: "red"}}> Next </NavLink>
-
-              <Switch>
-                <Route exact path="/" component={Clock} />
-                <Route path="/Next" component={Next} />
-              </Switch> */}
               <Form />
             </div>
-        </BrowserRouter>
         );
     }
 }
